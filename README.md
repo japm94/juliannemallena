fbbf133b9311da934fc79528ec92e8025b7bee9f
Ikaro <ikarosales7@gmail.com>
2018-04-30 09:19:40 -0300

    Path theme

--- README.md
+++
@@ -1,2 +0,0 @@
-# juliannemallena
-Julianne Mallena's Site
---
+++ juliannemallena/header.php
@@ -0,0 +1,66 @@
+<html>
+    <head>
+        <title>Julianne M. - MakeUp & Hair</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="Julianne M. - MakeUp & Hair">
+        <meta name="keywords" content="MakeUp, Hair">
+        <meta name="author" content="Júlio Melo">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <link rel="icon" type="image/png" href="<?= bloginfo('template_url'); ?>/assets/img/favicon.png"/>
+
+        <link href="<?= bloginfo('template_url'); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?= bloginfo('template_url'); ?>/assets/css/font-awesome.min.css" rel="stylesheet">
+        <link href="<?= bloginfo('template_url'); ?>/assets/css/et-line.css" rel="stylesheet">
+        <link href="<?= bloginfo('template_url'); ?>/assets/css/ionicons.min.css" rel="stylesheet">
+        <link href="<?= bloginfo('template_url'); ?>/assets/css/slick.css" rel="stylesheet">
+        <link href="<?= bloginfo('template_url'); ?>/assets/css/main.css" rel="stylesheet">
+        <link href="<?= bloginfo('template_url'); ?>/assets/css/magnific-popup.css" rel="stylesheet" >
+        <link href="<?= bloginfo('template_url'); ?>/assets/css/animate.min.css" rel="stylesheet">
+    </head>
+    <body>
+    <div class="loader">
+        <div class="loader-outter"></div>
+        <div class="loader-inner"></div>
+    </div>
+    <div class="body-container container-fluid">
+        <a class="menu-btn" href="javascript:void(0)">
+            <i class="ion ion-grid"></i>
+        </a>
+        <div class="row justify-content-center">
+            <div class="col-lg-2 col-md-3 col-12 menu_block">
+                <div class="logo_box">
+                    <a href="#"><img src="<?= bloginfo('template_url'); ?>/assets/img/logo.png" alt="cocoon"></a>
+                </div>
+                <div class="side_menu_section">
+                    <ul class="menu_nav">
+                        <li class="active"><a href="index.html">Home</a></li>
+                        <li><a href="about.html">About Us</a></li>
+                        <li><a href="services.html">Services</a></li>
+                        <li><a href="portfolio.html">Portfolio</a></li>
+                        <li><a href="blog.html">Cursos</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="side_menu_section">
+                    <h4 class="side_title">filter by:</h4>
+                    <ul  id="filtr-container"  class="filter_nav">
+                        <li  data-filter="*" class="active"><a href="javascript:void(0)" >all</a></li>
+                        <li data-filter=".branding"> <a href="javascript:void(0)">branding</a></li>
+                        <li data-filter=".design"><a href="javascript:void(0)">design</a></li>
+                        <li data-filter=".photography"><a href="javascript:void(0)">photography</a></li>
+                        <li data-filter=".architecture"><a href="javascript:void(0)">architecture</a></li>
+                    </ul>
+                </div>
+                <div class="side_menu_bottom">
+                    <div class="side_menu_bottom_inner">
+                        <ul class="social_menu">
+                            <li><a href="#"><i class="ion ion-social-facebook"></i></a></li>
+                            <li><a href="#"><i class="ion ion-social-twitter"></i></a></li>
+                        </ul>
+                        <div class="copy_right">
+                            <p class="copyright">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://Júlio Melo.com" target="_blank">Júlio Melo</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
